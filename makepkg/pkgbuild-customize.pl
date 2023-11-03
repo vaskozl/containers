091d0819d79a552c3d24737ed78e2d66f04e88bd
Vasil Zlatanov <v@skozl.com>
2023-11-03 21:11:53 +0000

    Increase yarn network-timeout

--- makepkg/pkgbuild-customize.pl
+++ makepkg/pkgbuild-customize.pl
@@ -2,4 +2,4 @@
 # Build for any Arch
 s/^arch=.*/arch=('any')/;
 # Increase yarn install timeout
-s/yarn\s+install\s+.*(?<!--network-timeout)/$& --network-timeout 600000/;
+s/yarn\s+install\s+.*(?<!--network-timeout)/$& --network-timeout 1200000/;
